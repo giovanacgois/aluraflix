82a472ed5eb5bc3bf6f9da3f2ca3d04348ac5fa6
Giovana Carolina de Gois <giovanacgois@gmail.com>
2021-07-19 23:55:43 -0300

    Creates the project structure

---
+++ src/main/java/com/giovanacgois/aluraflix/AluraflixApplication.java
@@ -0,0 +1,13 @@
+package com.giovanacgois.aluraflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraflixApplication.class, args);
+	}
+
+}
